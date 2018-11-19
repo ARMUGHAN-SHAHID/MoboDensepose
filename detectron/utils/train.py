0c97e4edb683cd5e541401477d62c90b37c2ad2a
marmughanshahid@gmail.com <marmughanshahid@gmail.com>
2018-11-19 10:59:37 +0500

    debug

--- detectron/utils/train.py
+++ detectron/utils/train.py
@@ -125,8 +125,8 @@ def create_model():
     if cfg.MEMONGER:
         optimize_memory(model)
     # Performs random weight initialization as defined by the model
-    # workspace.RunNetOnce(model.param_init_net)
-    workspace.RunNetOnce(model.XavierInit)
+    workspace.RunNetOnce(model.param_init_net)
+    # workspace.RunNetOnce(model.XavierInit)
     return model, weights_file, start_iter, checkpoints, output_dir
 
 
